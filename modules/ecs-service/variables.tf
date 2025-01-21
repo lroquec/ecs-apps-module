6a8b63ee6f37397690d8bdb1421ab2ed36230c42
Laura Roque <lroquecu@gmail.com>
2025-01-21 16:11:42 +0100

    Add ALB DNS variables to ECS service module

--- modules/ecs-service/variables.tf
+++ modules/ecs-service/variables.tf
@@ -94,3 +94,13 @@ variable "subdomain" {
   description = "The subdomain for the service"
   type        = string
 }
+
+variable "alb_dns_name" {
+  description = "DNS name of the ALB"
+  type        = string
+}
+
+variable "alb_zone_id" {
+  description = "Zone ID of the ALB"
+  type        = string
+}
