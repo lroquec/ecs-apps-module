0a2efba8df21b64e3085f37f0ab55e283c3bc26c
Laura Roque <lroquecu@gmail.com>
2025-01-22 07:44:53 +0100

    Add ALB default listener with fixed response for unconfigured routes

--- modules/ecs-cluster/main.tf
+++ modules/ecs-cluster/main.tf
@@ -40,3 +40,19 @@ resource "aws_lb" "main" {
 
   tags = var.tags
 }
+
+# ALB Default Listener (NUEVO)
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "No routes configured"
+      status_code  = "404"
+    }
+  }
+}
