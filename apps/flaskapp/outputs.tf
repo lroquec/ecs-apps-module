4dea0373ee0168af47c9ddfd4372de1642c72e4d
Laura Roque <lroquecu@gmail.com>
2025-01-22 07:44:11 +0100

    Remove Flask and Nginx application Terraform configurations and associated files

--- apps/flaskapp/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_url" {
-  description = "URL where the application is accessible"
-  value       = "http://${module.flask_service.dns_name}"
-}
-
-output "dns_name" {
-  description = "DNS name of the service"
-  value       = module.flask_service.dns_name
-}
